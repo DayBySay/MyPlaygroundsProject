d6c9d0563911ff979d2f7141c6c6c1ccb8a472f3
daybysay <yukita.sekai+github@gmail.com>
2019-12-08 13:11:35 +0900

    add carthage

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,4 +1,51 @@
 import UIKit
 import App
+import RxSwift
 
-Log().hello()
+enum HogeError: Error {
+    case errordayo
+}
+
+let ob = Observable<String>.create { (observer) -> Disposable in
+    observer.onNext("hoge")
+    observer.onNext("fuga")
+    observer.onError(HogeError.errordayo)
+    observer.onNext("nyassu")
+    observer.onCompleted()
+    observer.onNext("uhyo-")
+    return Disposables.create()
+}
+
+ob.subscribe(onNext: { (str) in
+    print("next: \(str)")
+}, onError: { (error) in
+    print("error: \(error)")
+}, onCompleted: {
+    print("completed")
+}) {
+    print("disposed")
+}
+
+let materialized = ob.materialize()
+let elements = materialized.filter { $0.element != nil }.map { $0.element! }
+let error = materialized.filter { $0.error != nil }.map { $0.error! }
+
+elements.subscribe(onNext: { (str) in
+    print("elements next: \(str)")
+}, onError: { (error) in
+    print("elements error: \(error)")
+}, onCompleted: {
+    print("elements completed")
+}) {
+    print("elements disposed")
+}
+
+error.subscribe(onNext: { (str) in
+    print("error next: \(str)")
+}, onError: { (error) in
+    print("error error: \(error)")
+}, onCompleted: {
+    print("error completed")
+}) {
+    print("error disposed")
+}
